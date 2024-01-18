b7a0fbb166373f2ac11c3ce26b27ef4667753813
Yelk11 <neighbour.matthew@gmail.com>
2024-01-18 16:44:01 -0500

    added arrays

--- 00_arrays/array.c
+++ 00_arrays/array.c
@@ -1,3 +1,5 @@
+#include "stdio.h"
+
 
 
 int main()
@@ -5,10 +7,28 @@ int main()
     // 1 dimensional array
     int one_dim_arr[9] = {0,1,2,3,4,5,6,7,8};
 
+    // print
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d\n", one_dim_arr[i]);
+    }
+
     // multidimensional array [ROW] [COLUMN]
     int multi_dim_arr[3][4] = {
         {0,1,2,3}, 
         {4,5,6,7}, 
         {8,9,10,11}
     };
+
+    // print
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            printf("%d\n", multi_dim_arr[x][y]);
+        }
+        printf("\n");
+    }
+    
 }
+
--- Makefile
+++ Makefile
@@ -2,6 +2,11 @@
 
 
 
-array: main.o dbAdapter.o
-   gcc -o program main.o dbAdapter.o
+array: 00_arrays/array.c
+	clang 00_arrays/array.c
+
+linkedlist: 01_lists/linked_list.c
+	clang 01_lists/linked_list.c
+
+
 	
