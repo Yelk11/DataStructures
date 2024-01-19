46fd76488a3cf8497380477b13503866ad3e04ea
Yelk11 <neighbour.matthew@gmail.com>
2024-01-18 20:32:40 -0500

    added linked list

--- 01_lists/linked_list.c
+++ 01_lists/linked_list.c
@@ -1,12 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node_t{
+    struct node_t* next_node;
+    int value;
+}node_t;
+
+node_t* init_list(int value)
+{
+    node_t* node = calloc(1, sizeof(node_t));
+    node->value = value;
+    node->next_node = NULL;
+    return node;
+}
+
+node_t* add_link(node_t* linked_list, int value) 
+{
+    node_t* node = init_list(value);
+    if (linked_list != NULL)
+    {
+        while (linked_list->next_node != NULL)
+        {
+            linked_list = linked_list->next_node;
+        }
+        linked_list->next_node = node;
+        
+    }
+    return linked_list;
+}
 
 
-
-
-
-
-
+void print_linked_list(node_t* node)
+{
+    while(node->next_node != NULL)
+    {
+        printf("%d\n", node->value);
+        node = node->next_node;
+    }
+    printf("%d\n", node->value);
+}
 
 int main()
 {
-
+    node_t* root = init_list(10);
+    add_link(root, 7);
+    add_link(root, 3);
+    add_link(root, 5);
+    add_link(root, 9);
+    print_linked_list(root);
 }
--- Makefile
+++ Makefile
@@ -1,12 +1,28 @@
 
 
+all: linkedlist array
 
 
+# arrays
 array: 00_arrays/array.c
-	clang 00_arrays/array.c
+	clang -o array 00_arrays/array.c 
 
+# linked list
 linkedlist: 01_lists/linked_list.c
-	clang 01_lists/linked_list.c
+	clang -o linked_list 01_lists/linked_list.c 
 
 
-	
+# binary tree
+
+# b-tree
+
+# heap
+
+# bit_slice_tree
+
+
+
+
+
+clean:
+	(@RM) array linked_list
